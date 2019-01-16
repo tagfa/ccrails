e32d24810c7a323f6a2d1b85805e770056328ff7
tagfa <tagfa.job@gmail.com>
2019-01-16 19:51:14 +0900

    Delete migration file
    Duplication column

--- db/migrate/20181116065747_add_detail_to_learningusers.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailToLearningusers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :learningusers, :namesei, :string
-    add_column :learningusers, :namemei, :string
-  end
-end
